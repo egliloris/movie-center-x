fddc27b386d9ed01f26be58d414b96400fe21afb
Loris Keller <loris.keller@zuehlke.com>
2025-01-24 07:39:36 +0100

    setup app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
+
+const materialModules = [MatButtonModule, MatToolbarModule];
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, materialModules],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'movie-center-x';
-}
+export class AppComponent {}
