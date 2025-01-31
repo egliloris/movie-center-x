e36f60c23430bea79ddd6a90d15aebd58f060e28
Loris Keller <loris.keller@zuehlke.com>
2025-01-31 14:59:58 +0100

    add active route css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { selectTitle, selectUserName } from './store/global.selectors';
 
@@ -17,7 +17,7 @@ interface NavItem {
 
 @Component({
   selector: 'app-root',
-  imports: [AsyncPipe, RouterOutlet, RouterLink, materialModules],
+  imports: [AsyncPipe, RouterOutlet, RouterLink, RouterLinkActive, materialModules],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
