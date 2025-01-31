c2d9a77b9b918a1ff36d052dbb7388b6c046f44b
Loris Keller <loris.keller@zuehlke.com>
2025-01-31 15:20:22 +0100

    remove badge as it throws html errors

--- src/app/features/movies/components/movie-item/movie-item.component.ts
+++ src/app/features/movies/components/movie-item/movie-item.component.ts
@@ -1,14 +1,12 @@
 import { DatePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
-import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
 import { Movie } from '../../models/movies.models';
 
 @Component({
   selector: 'app-movie-item',
-  imports: [DatePipe, MatCardModule, MatButtonModule, MatBadgeModule, MatIconModule],
+  imports: [DatePipe, MatButtonModule, MatCardModule],
   templateUrl: './movie-item.component.html',
   styleUrl: './movie-item.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
